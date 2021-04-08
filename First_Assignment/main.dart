786cd76418656decc3c8a1332e2c9304b14ecddb
Azim <azimdjan18@gmail.com>
2021-04-08 23:42:48 +0500

    chalenge 1 is done

--- First_Assignment/main.dart
+++ First_Assignment/main.dart
@@ -1,10 +1,15 @@
 import 'dart:math';
 
 void main() {
-  const x = 45;
-  const y = 55;
-  const answer1 = (x * 100) + y;
-  const answer2 = (x * 100) + (y * 100);
-  const answer3 = (x * 100) + (y / 10);
-  print(answer3);
+  const attendance = 20;
+  const homework = 30;
+  const exam = 50;
+  int attenPoint = 90;
+  int homePoint = 80;
+  int examPoint = 94;
+  int grade;
+  grade = (attenPoint * (attendance / 100) +
+      homePoint * (homework / 100) +
+      examPoint * (exam / 100)).toInt();
+  print("Student's grade is $grade%");
 }

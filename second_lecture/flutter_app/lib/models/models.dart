c14e1533519825b0aa76a1627e16b89e52cf3d69
Azimdjan <azimdjan18@gmail.com>
2021-07-03 11:12:19 +0500

    Unnecessary files were deleted

--- second_lecture/flutter_app/lib/models/models.dart
+++
@@ -1,8 +0,0 @@
-export 'explore_recipe.dart';
-export 'explore_data.dart';
-export 'post.dart';
-export 'simple_recipe.dart';
-export 'grocery_item.dart';
-export 'tabManager.dart';
-export 'tabManager.dart';
-export 'grocery_manager.dart';
